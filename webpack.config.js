bfd716ea11b2f5e09f4ce2882b38e079e528adbc
Yury Baykov <y.baykov@corp.mail.ru>
2021-07-06 22:04:04 +0300

    leave evals for dev builds

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = function (env, argv) {
     externals: {
       axios: "axios",
     },
-    devtool: false,
+    devtool: argv.mode === "production" ? false : 'eval',
     plugins: [],
   };
 };
